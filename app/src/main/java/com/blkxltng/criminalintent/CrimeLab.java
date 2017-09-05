78aea2d26f4cbc1e4b6b5948b5a7a5d536cf6b7c
blkxltng <firejama@gmail.com>
2017-09-05 19:06:33 -0400

    made crimelab more efficient

--- app/src/main/java/com/blkxltng/criminalintent/CrimeLab.java
+++ app/src/main/java/com/blkxltng/criminalintent/CrimeLab.java
@@ -3,7 +3,9 @@ package com.blkxltng.criminalintent;
 import android.content.Context;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 /**
@@ -14,7 +16,7 @@ public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
+    private Map<UUID, Crime> mCrimes;
 
     public static CrimeLab get(Context context) {
         if(sCrimeLab == null) {
@@ -24,27 +26,22 @@ public class CrimeLab {
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mCrimes = new LinkedHashMap<>();
 
         //Temporary to be removed later
         for(int i = 0; i < 100; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
             crime.setSolved(i % 2 == 0); //Every other crime
-            mCrimes.add(crime);
+            mCrimes.put(crime.getId(), crime);
         }
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>(mCrimes.values());
     }
 
     public Crime getCrime(UUID id) {
-        for(Crime crime: mCrimes) {
-            if(crime.getId().equals(id)) {
-                return crime;
-            }
-        }
-        return null;
+       return mCrimes.get(id);
     }
 }
