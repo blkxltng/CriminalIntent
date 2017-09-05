ff403d3473b13f7a0f1984d12a47ac0529282e43
blkxltng <firejama@gmail.com>
2017-09-05 17:49:16 -0400

    can now update individual crimes

--- app/src/main/java/com/blkxltng/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/blkxltng/criminalintent/CrimeListFragment.java
@@ -1,5 +1,6 @@
 package com.blkxltng.criminalintent;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,7 +11,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
@@ -22,6 +22,7 @@ public class CrimeListFragment extends Fragment {
 
     private RecyclerView mCrimeRecyclerView;
     private CrimeAdapter mAdapter;
+    private int mUpdatedPosition = -1;
 
     @Nullable
     @Override
@@ -36,12 +37,27 @@ public class CrimeListFragment extends Fragment {
         return  view;
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if(mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            if(mUpdatedPosition > -1) {
+                mAdapter.notifyItemChanged(mUpdatedPosition);
+                mUpdatedPosition = -1;
+            } else {
+                mAdapter.notifyDataSetChanged();
+            }
+        }
     }
 
     private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -69,7 +85,9 @@ public class CrimeListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(getActivity(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            Intent intent = CrimeActivity.newIntent(getActivity(), mCrime.getId());
+            mUpdatedPosition = this.getAdapterPosition();
+            startActivity(intent);
         }
     }
 
