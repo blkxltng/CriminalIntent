ddde01ee631104f0db721820a682338445bf038b
blkxltng <firejama@gmail.com>
2017-09-26 00:07:26 -0400

    Added the ability to take pictures to associate with a crime

--- app/src/main/java/com/blkxltng/criminalintent/CrimeLab.java
+++ app/src/main/java/com/blkxltng/criminalintent/CrimeLab.java
@@ -9,6 +9,7 @@ import com.blkxltng.criminalintent.database.CrimeBaseHelper;
 import com.blkxltng.criminalintent.database.CrimeCursorWrapper;
 import com.blkxltng.criminalintent.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -70,6 +71,11 @@ public class CrimeLab {
         } finally {
             cursor.close();
         }
+    }
+
+    public File getPhotoFile(Crime crime) {
+        File filesDir = mContext.getFilesDir();
+        return new File(filesDir, crime.getPhotoFilename());
     }
 
     public void updateCrime(Crime crime) {
